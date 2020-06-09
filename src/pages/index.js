56c04440ee1e2d0517df1918cc2f924b3f7e7239
Ömer Aksu <omeeraksu@gmail.com>
2020-06-09 21:47:24 +0300

    "coming soon" comit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '@common/Layout';
+
+import Header from '@sections/Header';
+
+const IndexPage = () => (
+  <Layout>
+    <Header />
+  </Layout>
+);
+
+export default IndexPage;
