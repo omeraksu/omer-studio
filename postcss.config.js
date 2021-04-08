63c7d24c11ea036a92a48334444e689f02a63774
omeraksu <omeeraksu@gmail.com>
2021-04-08 09:47:36 +0300

    tailwind css add

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   plugins: {
-    "postcss-nested": {},
-    "postcss-custom-media": {
-      importFrom: [
-        {
-          customMedia: { "--t": "(min-width: 544px)" },
-        },
-        {
-          customMedia: { "--d": "(min-width: 1270px)" },
-        },
-      ],
-    },
+    "@tailwindcss/jit": {},
+    autoprefixer: {},
   },
 };
